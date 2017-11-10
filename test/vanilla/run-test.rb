856430ea5351b7b53b21cd9b783a74dd59d95c85
Dai MIKURUBE <dmikurube@treasure-data.com>
2017-11-10 23:42:02 +0900

    [WIP] Locate embedded gems under gems/ in the .jar file

--- test/vanilla/run-test.rb
+++ test/vanilla/run-test.rb
@@ -5,6 +5,14 @@ test_dir = File.join(base_dir, "test")
 $LOAD_PATH.unshift(lib_dir)
 $LOAD_PATH.unshift(test_dir)
 
+# This `require` makes gems embedded in embulk-core.jar available to be required.
+require "#{base_dir}/classpath/embulk-core-0.8.36.jar"
+
+# This Gem.path trick is needed to load gems embedded in embulk-core.jar when run through jruby-gradle-plugin.
+# It can be after `require "embulk-core.jar"`.
+Gem.path << 'uri:classloader://'
+Gem::Specification.reset
+
 require "test/unit"
 
 Dir.glob("#{base_dir}/test/vanilla/**/test{_,-}*.rb") do |file|
